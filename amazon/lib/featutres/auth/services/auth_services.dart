66b549de216adc214478f47edd977f2b5a7d7fae
firas foued <firas.fouad@isimg.tn>
2024-07-08 16:07:28 +0100

    a9dhily

--- amazon/lib/featutres/auth/services/auth_services.dart
+++ amazon/lib/featutres/auth/services/auth_services.dart
@@ -4,49 +4,43 @@ import 'package:amazon/constants/utils.dart';
 import 'package:amazon/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-class AuthService{
 
-  //sign up user
+class AuthService {
+  // Sign up user
   void signUpUser({
     required BuildContext context,
     required String email,
-    required String password ,
-    required String name ,
-  })
-  async {
-    try{
+    required String password,
+    required String name,
+  }) async {
+    try {
       User user = User(
         id: '',
-        name: name, 
-        email: email, 
-        password: password, 
-        address: '', 
-        type: '', 
+        name: name,
+        email: email,
+        password: password,
+        address: '',
+        type: '',
         token: ''
-        );
-      http.Response res =await   http.post(
+      );
+
+      http.Response res = await http.post(
         Uri.parse('$uri/api/signup'),
-        body: user.toJson(), 
-        headers: <String , String >{
-          'Content-Type' :'application/json; charset=UTF8',
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8', 
         },
+      );
 
-        );
-        httpErrorHandle(
-          response: res,
-         context: context,
-          onSuccess: (){
-            showSnackBar(context, 'Account created ! Login with the same credentials!  ',);
-
-          });
-
-    }catch (e){
-      showSnackBar(context, e.toString() ); 
-    
-
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(context, 'Account created! Login with the same credentials!');
+        }
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
     }
-
-
   }
-
 }
