4f9cf8aec39e858186410200aaf167e2ef347108
firas foued <firas.fouad@isimg.tn>
2024-08-06 21:30:49 +0100

    a9dhily

--- lib/featutres/home/screens/home_screens.dart
+++ lib/featutres/home/screens/home_screens.dart
@@ -1,4 +1,5 @@
 import 'package:amazon/constants/global_variables.dart';
+import 'package:amazon/featutres/home/widgets/address_box.dart';
 
 import 'package:flutter/material.dart';
 
@@ -18,82 +19,75 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60),
+     appBar: PreferredSize(
+
+        preferredSize: const Size.fromHeight(60) ,
         child: AppBar(
           flexibleSpace: Container(
-            decoration: const BoxDecoration(
+            decoration: const BoxDecoration
+            (
               gradient: GlobalVariables.appBarGradient,
-            ),
-          ),
+
+            ) ,
+          ) ,
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Expanded(
-                child: Container(
-                  height: 42,
-                  margin: const EdgeInsets.only(left: 15),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(7),
-                    elevation: 1,
-                    child: TextFormField(
-                      onFieldSubmitted: navigateToSearchScreen,
-                      decoration: InputDecoration(
-                        prefixIcon: InkWell(
-                          onTap: () {},
-                          child: const Padding(
-                            padding: EdgeInsets.only(
-                              left: 6,
-                            ),
-                            child: Icon(
-                              Icons.search,
-                              color: Colors.black,
-                              size: 23,
-                            ),
-                          ),
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        contentPadding: const EdgeInsets.only(top: 10),
-                        border: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(7),
-                          ),
-                          borderSide: BorderSide.none,
-                        ),
-                        enabledBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(7),
-                          ),
-                          borderSide: BorderSide(
-                            color: Colors.black38,
-                            width: 1,
-                          ),
-                        ),
-                        hintText: 'Search Amazon.in',
-                        hintStyle: const TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 17,
-                        ),
+            Expanded(
+              child: Container(
+                height: 42,
+                margin: const EdgeInsets.only(left: 15),
+                child:Material(
+                  borderRadius: BorderRadius.circular(7),
+                  elevation: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: InkWell(
+                        onTap: (){},
+                        child: const Padding(
+                          padding: EdgeInsets.only(left: 6),
+                          child: Icon(Icons.search, color: Colors.black,size: 23,),),
+
                       ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: const EdgeInsets.only(top: 10),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(7),),
+                        borderSide: BorderSide.none,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(7),),
+                        borderSide: BorderSide(color: Colors.black38, width: 1),
+                      ),
+                      hintText: 'Search Amazon.In',
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
+                      )
                     ),
                   ),
-                ),
+                 ) ,
               ),
-              Container(
-                color: Colors.transparent,
-                height: 42,
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                child: const Icon(Icons.mic, color: Colors.black, size: 25),
-              ),
-            ],
+            ),
+            Container(
+              color: Colors.transparent,
+              height: 42,
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Icon(Icons.mic , color: Colors.black,size: 25,),
+
+
+            ),
+             
+            
+           ]
           ),
         ),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Column(
-          children: const [
-         
+          children: [
+AddressBox(),         
           ],
         ),
       ),
